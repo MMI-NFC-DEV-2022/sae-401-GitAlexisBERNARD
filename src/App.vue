4206d0e52ba2aa0bd66453f81e00ca3cbb85fc7a
GitAlexisBERNARD <alexis.bernard02@edu.univ-fcomte.fr>
2024-03-19 10:12:10 +0100

    Correction bug build

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,9 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 import { Suspense } from 'vue'
-import loginlogout from '@/components/loginlogout.vue'
 </script>
 
 <template>
-  <header>
-    <loginlogout />
-  </header>
   <main>
     <Suspense>
       <RouterView />
