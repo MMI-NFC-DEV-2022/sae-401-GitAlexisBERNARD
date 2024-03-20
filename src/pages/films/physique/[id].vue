94bbea9692b7f644b5ae2159bc6ff87b7d7101d4
GitAlexisBERNARD <alexis.bernard02@edu.univ-fcomte.fr>
2024-03-20 15:40:35 +0100

    Ajout Css page physique

--- src/pages/films/physique/[id].vue
+++ src/pages/films/physique/[id].vue
@@ -14,15 +14,28 @@ if (ErrorPlatformePhysique) {
 }
 </script>
 <template>
-  <h2 class="font-bold font-Spline ml-5 text-xl mt-6 mb-4">Acheter {{ PlatformePhysique![0]!.FILM!.Titre }}</h2>
-  <ul class="flex flex-col">
-    <li v-for="nPhysique in PlatformePhysique" :key="nPhysique.id ?? undefined" class="flex">
-      <img :src="nPhysique.url_images ?? undefined" :alt="nPhysique.NomPhysique ?? undefined" />
-      <div>
-        <p>{{ nPhysique.NomPhysique }}</p>
-        <p>{{ nPhysique.Type }}</p>
-        <p>{{ nPhysique.codec }}</p>
-        <p>{{ nPhysique.AspectRatio }}</p>
+  <h2 class="font-bold font-Spline ml-5 text-xl mt-6 mb-4">
+    Acheter {{ PlatformePhysique![0]!.FILM!.Titre }}
+  </h2>
+  <ul class="flex flex-col ml-5 gap-6">
+    <li
+      v-for="nPhysique in PlatformePhysique"
+      :key="nPhysique.id ?? undefined"
+      class="flex flex-row gap-5"
+    >
+      <img
+        :src="nPhysique.url_images ?? undefined"
+        :alt="nPhysique.NomPhysique ?? undefined"
+        class="w-[120px] h-[160px]"
+      />
+      <div class="flex flex-col gap-[5px]">
+        <p class="font-Spline text-normal text-[24px]">{{ nPhysique.NomPhysique }}</p>
+        <p class="font-Spline text-normal text-[14px]">{{ nPhysique.Type }}</p>
+        <p class="font-Spline text-normal text-[14px] text-[#405473]">{{ nPhysique.Resolution }}</p>
+        <p class="font-Spline text-normal text-[14px] text-[#405473]">{{ nPhysique.codec }}</p>
+        <p class="font-Spline text-normal text-[14px] text-[#405473]">
+          Aspect ratio: {{ nPhysique.AspectRatio }}
+        </p>
       </div>
     </li>
   </ul>
