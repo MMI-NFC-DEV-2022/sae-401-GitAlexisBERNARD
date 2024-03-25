0f40e883a4c7983d792acfa7477b9b5794067435
GitAlexisBERNARD <alexis.bernard02@edu.univ-fcomte.fr>
2024-03-25 15:09:08 +0100

    ajout connexion via github

--- src/pages/films/genre/[id].vue
+++ src/pages/films/genre/[id].vue
@@ -1,15 +1,17 @@
 <script setup lang="ts">
 import { supabase } from '@/supabase'
 const props = defineProps<{
-  genre?: number
+  id?: number
 }>()
+console.log(props.id)
 const { data: FILM, error } = await supabase
-  .from('FILMGenre')
-  .select('FILM(*)')
-  .eq('Genre')
-  .limit(props.max ?? 100)
+  .from('FilmGenre')
+  .select('GENRE(Nom),FILM(*)')
+  .eq('id_genre', props.id)
+console.log(FILM)
 </script>
 <template>
+  <h2 class="font-Spline font-medium text-[24px]">Genre {{ FILM[0]?.GENRE.Nom }}</h2>
   <div class="flex overflow-auto ml-5">
     <ul
       class="lg:flex-wrap flex lg:justify-center items-center flex-nowrap snap-x snap-mandatory scroll-smooth gap-3"
@@ -18,12 +20,12 @@ const { data: FILM, error } = await supabase
         <RouterLink
           :to="{
             name: 'films-id',
-            params: { id: nFILM.id }
+            params: { id: nFILM.FILM.id }
           }"
           class="block"
         >
-          <img :src="nFILM.url_images ?? ''" alt="nFILM.Titre" class="rounded-xl" />
-          <p class="font-Spline font-medium text-center text-[16px]">{{ nFILM.Titre }}</p>
+          <img :src="nFILM.FILM.url_images ?? ''" alt="nFILM.Titre" class="rounded-xl" />
+          <p class="font-Spline font-medium text-center text-[16px]">{{ nFILM.FILM.Titre }}</p>
         </RouterLink>
       </li>
     </ul>
